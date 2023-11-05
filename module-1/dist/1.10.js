0a7c35a78b18dbf33b462a114699a516e4ea11ec
hasankarim18 <hallysust@gmail.com>
2023-11-05 13:18:51 +0600

    ternary, optional chaining, nullish coalescing operator

---
+++ module-1/dist/1.10.js
@@ -0,0 +1,16 @@
+"use strict";
+{
+    const newDeveloper = 'juniorDeveloper';
+    const userOne = {
+        name: "Dabul",
+        // email: "dabul@mail.com",
+        gender: "male",
+        bloodGroup: "A"
+    };
+    const mernDeveloper = {
+        skills: ['Html', 'css', 'express'],
+        designation1: 'BackendDeveloper',
+        designation2: "ReactDeveloper"
+    };
+    // 
+}
---
+++ module-1/dist/1.9.js
@@ -0,0 +1,26 @@
+"use strict";
+{
+    const student1 = {
+        name: "Hasan",
+        age: 30,
+        gender: "male",
+        contactNo: '01700000000',
+        address: 'Dhk'
+    };
+    const student2 = {
+        name: 'Karim',
+        age: 20,
+        gender: "male",
+        address: "Ctg"
+    };
+    const student3 = {
+        name: 'Karim',
+        age: 20,
+        gender: "male",
+        address: "Ctg"
+    };
+    const userName = "Mahbub";
+    const isAdmin = false;
+    const add = (num1, num2) => num1 + num2;
+    //
+}
